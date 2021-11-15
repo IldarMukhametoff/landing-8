8cf2dccee841de24e792bfca79bc7bfb9f094de7
IldarMukhametoff <77933697+IldarMukhametoff@users.noreply.github.com>
2021-11-15 23:52:19 +0500

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,4 @@
-new Swiper('.slider',{
+new Swiper('.intro__slider',{
 	navigation:{
 		nextEl:'.swiper-button-next',
 		prevEl:'.swiper-button-prev'
@@ -7,4 +7,30 @@ new Swiper('.slider',{
 		el:'.swiper-pagination',
 		clickable:true,
 	},
+});
+new Swiper('.testimonials__slider',{
+	pagination:{
+		el:'.swiper-pagination',
+		clickable:true,
+	},
+});
+new Swiper('.brands__slider',{
+	navigation:{
+		nextEl:'.swiper-button-next',
+		prevEl:'.swiper-button-prev'
+	},
+	breakpoints:{
+		1024:{
+			slidesPerView:6,
+		},
+		767:{
+			slidesPerView:4,
+		},
+		480:{
+			slidesPerView:2,
+		},
+		320:{
+			slidesPerView:1,
+		}
+	}
 });
